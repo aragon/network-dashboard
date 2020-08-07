f179c304128614cca23eebba3eac7fa152ae31c1
Andy Hook <hello@andy-hook.co.uk>
2020-08-07 17:09:01 +0100

    Remove old Connect provider

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,40 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Connect } from '@aragon/connect-react'
 import Header from './Header/Header'
 
 function MainView({ children }) {
   return (
-    <Connect
-      location="agreement3.aragonid.eth"
-      connector="thegraph"
-      options={{ network: 4 }}
+    <div
+      css={`
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+      `}
     >
       <div
         css={`
-          display: flex;
-          flex-direction: column;
-          height: 100vh;
+          flex-shrink: 0;
         `}
       >
-        <div
-          css={`
-            flex-shrink: 0;
-          `}
-        >
-          <Header />
-        </div>
-        <main
-          css={`
-            flex-grow: 1;
-            flex-shrink: 1;
-            height: 0;
-          `}
-        >
-          {children}
-        </main>
+        <Header />
       </div>
-    </Connect>
+      <main
+        css={`
+          flex-grow: 1;
+          flex-shrink: 1;
+          height: 0;
+        `}
+      >
+        {children}
+      </main>
+    </div>
   )
 }
 
