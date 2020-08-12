9c0c78ab43d131fde82e7fdaeed77ac5c0c055ef
delfipolito <delfinapolito@gmail.com>
2020-08-12 17:19:12 -0300

    Modify onClick card function

--- src/components/Proposals/ProposalCard.js
+++ src/components/Proposals/ProposalCard.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import { Card, GU } from '@aragon/ui'
 
 function ProposalCard({ vote, onProposalClick }) {
-  const { voteId, context } = vote
+  const { voteId, id, context } = vote
 
   return (
     <Card
-      onClick={() => onProposalClick(voteId)}
+      onClick={() => onProposalClick(id)}
       css={`
         padding: ${3 * GU}px;
       `}
