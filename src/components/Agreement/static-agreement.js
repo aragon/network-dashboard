04b1cdb3069b489a24a1dc6edabbefa648a4d530
Andy Hook <hello@andy-hook.co.uk>
2020-08-18 17:07:34 +0100

    Add agreement document with static MD

---
+++ src/components/Agreement/static-agreement.js
@@ -0,0 +1,65 @@
+export default `
+# Aragon Network Agreement
+
+### 0. Background
+
+The Aragon Network is an internet-based jurisdiction that provides reliable dispute resolution services and other critical infrastructure to individuals and organizations.
+
+The Aragon Network is governed by Aragon Network Token (ANT) holders, who supported the initial creation of the jurisdiction and now continue to guide its evolution through established governance processes.
+
+This Agreement exists to enumerate the shared mission, values, and constraints that bind ANT holders in their collaborative effort to create value for each other and for Aragon Network service subscribers.
+Section 1. RFC 2119 compliance
+The key words "MUST", "MUST NOT", "REQUIRED", "SHALL", "SHALL NOT", "SHOULD", "SHOULD NOT", "RECOMMENDED", "MAY", and "OPTIONAL" in this document are to be interpreted as described in RFC 2119.
+
+Section 2. Immutable guidelines
+
+The guidelines in this section cannot be changed. If any other guidelines conflict with these immutable guidelines, the immutable guidelines shall take priority for enforcement purposes.
+
+### 1. ANT holder guidelines
+
+These guidelines apply to all holders of ANT.
+
+ANT holders SHALL NOT hold any other ANT holder liable in any jurisdiction other than the Aragon Network and Aragon Court for any action taken in the Aragon Network.
+
+In case of any dispute between one or more ANT holders and one or more other ANT holders pertaining to this Agreement and related action taken in the Aragon Network, such dispute SHALL be settled in Aragon Court according to this Agreement. ANT holders SHALL NOT attempt to settle such dispute in any venue other than Aragon Court, and any venue that hears such dispute SHOULD dismiss the dispute on the basis of this Agreement or otherwise uphold the final ruling rendered by Aragon Court.
+
+### 2. Proposal guidelines
+
+These guidelines apply to all proposals put forth to the Aragon Network organization.
+
+Proposals MUST be governed by this Aragon Agreement, or a future version of this Agreement as modified within the bounds of the Agreement, and disputes over a proposal MUST be resolved by Aragon Court. This guideline cannot be changed.
+
+Proposals MUST NOT disproportionately benefit a majority of ANT holders over the minority, as measured by token-weighted balances at the time the proposal vote starts. This guideline cannot be changed.
+
+Proposals MUST NOT have a YES vote cast for them by the Agent app address of the Aragon Network organization, or any contract installed on the Aragon Network organization that fulfills similar functionality as the Agent app. This guideline cannot be changed.
+
+Proposals MUST NOT have a YES vote cast for them by the address 0xcafe1a77e84698c83ca8931f54a755176ef75f2c or any other address owned by the Aragon Association. This guideline cannot be changed.
+
+Section 3. Mutable guidelines
+
+The guidelines in this section can be changed.
+
+### 3. Proposal guidelines
+
+These guidelines apply to all proposals put forth to the Aragon Network organization.
+
+Proposals MUST be consistent with the Aragon Manifesto.
+
+Proposals to transfer assets from the Aragon Network organization to the exclusive control of any other entity SHALL NOT be allowed until Ethereum block #15,000,000.
+
+Proposals MUST be accompanied by an accurate American English-language justification (“Justification”) of the proposal. Typos in the Justification SHALL be allowed if they do not substantially alter the meaning of the proposal. This Justification MUST be referred to in the on-chain source code or transaction data of the proposal by the content-address location of the Justification on the public IPFS network, and the Justification MUST be accessible via the public IPFS network for as long as the proposal is open for voting and/or in effect. If a Justification becomes temporarily inaccessible via the public IPFS network, and its proposal is challenged under this guideline, then jurors SHOULD block the proposal if they have reason to believe the Justification was inaccessible at the time that the proposal was challenged (give or take an hour, accounting for blockchain confirmation times). Alternatively, the Justification MAY be encoded directly into the on-chain source code or transaction data of the proposal so that the Justification remains human-readable and accessible as long as the Ethereum blockchain itself remains publicly available. The Justification MUST include the following information:
+
+The name(s) and/or username(s) of the author(s) of the proposal. The author(s) SHOULD be the primary point of contact for any questions or comments regarding the proposal.
+
+The author’s preferred contact method, in case anyone has questions or comments regarding the proposal.
+
+An accurate summary of the proposal in 280 characters or less.
+
+A rationale section detailing the author’s reason(s) for creating the proposal.
+
+An accurate, long-form description of what the proposal will do if enacted.
+
+A section acknowledging by title and author(s) any similar or related prior work known to the proposal author(s).
+
+If the proposal changes one or more blockchain rules or the blockchain state of the Aragon Network, then a security audit report of the blockchain rule or blockchain state change MUST be included in the Justification. This security audit report MUST be produced by a third-party security auditor and remain publicly accessible for as long as the change is in effect. The third-party security auditor MUST NOT have any financial relationship to the proposal author(s) aside from their being hired to perform the security audit.
+`
