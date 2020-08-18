7e7363821e21aca8d274cb5bfb38ce64fc5100d6
Andy Hook <hello@andy-hook.co.uk>
2020-08-18 09:43:13 +0100

    Add simple layout scaffold (#11)
    * Add ViewportProvider
    
    * Add layout scaffold with limiting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { GU } from '@aragon/ui'
+import { useTheme, GU } from '@aragon/ui'
 
 function Header() {
+  const theme = useTheme()
+
   return (
     <header
       css={`
-        display: inline-grid;
-        grid-auto-flow: column;
-        grid-gap: ${3 * GU}px;
+        background-color: ${theme.surface};
+        padding: ${2 * GU}px;
       `}
     >
-      <Link to="/proposals">Proposals</Link>
-      <Link to="/agreement">Agreement</Link>
+      <nav
+        css={`
+          display: inline-grid;
+          grid-auto-flow: column;
+          grid-gap: ${3 * GU}px;
+        `}
+      >
+        <Link to="/proposals">Proposals</Link>
+        <Link to="/agreement">Agreement</Link>
+      </nav>
     </header>
   )
 }
