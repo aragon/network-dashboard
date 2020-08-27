0a9d81f42af43db130396d9c51818c3e863b5ac9
Andy Hook <hello@andy-hook.co.uk>
2020-08-27 22:15:42 +0100

    Add proposal loading indicator

--- src/components/Proposals/ProposalLoading.js
+++ src/components/Proposals/ProposalLoading.js
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Box, textStyle, LoadingRing, GU } from '@aragon/ui'
+import loadingGraphic from '../../assets/loading.svg'
 
 function ProposalLoading() {
-  return <div>...Loading</div>
+  return (
+    <Box>
+      <div
+        css={`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 400px;
+          background-color: white;
+        `}
+      >
+        <img
+          src={loadingGraphic}
+          alt=""
+          css={`
+            display: block;
+            max-width: 100%;
+            width: ${34 * GU}px;
+          `}
+        />
+
+        <span
+          css={`
+            display: flex;
+            align-items: center;
+            margin-top: ${4 * GU}px;
+          `}
+        >
+          <div
+            css={`
+              margin-right: 16px;
+            `}
+          >
+            <LoadingRing mode="half-circle" />
+          </div>
+          <h1
+            css={`
+              ${textStyle('title3')}
+              line-height: 1.2;
+            `}
+          >
+            Loading proposal…
+          </h1>
+        </span>
+      </div>
+    </Box>
+  )
 }
 
 export default ProposalLoading
