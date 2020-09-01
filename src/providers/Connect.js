62576222ce78cc66b47e18d14b64f62c84328748
Andy Hook <hello@andy-hook.co.uk>
2020-09-01 13:22:52 +0100

    Environment: Use hosted org subgraph on ethereum (#66)

--- src/providers/Connect.js
+++ src/providers/Connect.js
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types'
 import { Connect } from '@aragon/connect-react'
 import { networkEnvironment } from '../current-environment'
 
+const ORG_SUBGRAPH_URL = networkEnvironment.subgraphs?.organization
+
+const connecterConfig = ORG_SUBGRAPH_URL && [
+  'thegraph',
+  { orgSubgraphUrl: ORG_SUBGRAPH_URL },
+]
+
 function ConnectProvider({ children }) {
   const { legacyNetworkType, chainId, orgLocation } = networkEnvironment
 
   return (
     <Connect
       location={orgLocation}
-      connector="thegraph"
+      connector={connecterConfig || 'thegraph'}
       options={{
         name: legacyNetworkType,
         network: chainId,
