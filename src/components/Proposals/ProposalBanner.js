d976ae5bdcd72270c0a8d3dc8cf2030c7a9cc30e
Andy Hook <hello@andy-hook.co.uk>
2020-09-02 08:22:27 +0100

    Small layout adjustment

--- src/components/Proposals/ProposalBanner.js
+++ src/components/Proposals/ProposalBanner.js
@@ -2,109 +2,107 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
   ButtonIcon,
-  GU,
   IconCross,
   Link,
   textStyle,
   useLayout,
   useTheme,
+  GU,
 } from '@aragon/ui'
-import pcBanner from '../../assets/pc-banner.svg'
 import LayoutGutter from '../Layout/LayoutGutter'
 import LayoutLimiter from '../Layout/LayoutLimiter'
+import pcSvgBanner from '../../assets/pc-banner.svg'
 
 function ProposalBanner({ onCloseBanner, ...props }) {
   const theme = useTheme()
   const { layoutName } = useLayout()
-  const compactMode =
-    layoutName === 'medium' || layoutName === 'small' || layoutName === 'min'
+  const wideMode = layoutName === 'large' || layoutName === 'max'
 
   return (
-    <div
+    <LayoutGutter
+      collapseWhenSmall={false}
       css={`
         background: linear-gradient(241.18deg, #ffb36d 6.22%, #ff8888 95.3%);
-        width: 100%;
-        background-size: cover;
-        padding-top: ${8 * GU}px;
-        padding-bottom: ${8 * GU}px;
+        padding-top: ${7 * GU}px;
+        padding-bottom: ${7 * GU}px;
+        position: relative;
       `}
       {...props}
     >
-      <LayoutGutter collapseWhenSmall={false}>
-        <LayoutLimiter>
-          <ButtonIcon
-            label=""
-            css={`
-              position: absolute;
-              top: ${4 * GU}px;
-              right: ${compactMode ? 2 * GU : 4 * GU}px;
-            `}
-            onClick={onCloseBanner}
-          >
-            <IconCross
+      <ButtonIcon
+        label=""
+        css={`
+          position: absolute;
+          top: ${wideMode ? 4 * GU : 2 * GU}px;
+          right: ${wideMode ? 3.5 * GU : 2 * GU}px;
+        `}
+        onClick={onCloseBanner}
+      >
+        <IconCross
+          css={`
+            color: ${theme.surface};
+          `}
+        />
+      </ButtonIcon>
+      <LayoutLimiter>
+        <div
+          css={`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 ${wideMode ? '5%' : '0'};
+          `}
+        >
+          <div>
+            <h1
               css={`
-                color: ${theme.surface};
-              `}
-            />
-          </ButtonIcon>
-          <div
-            css={`
-              display: flex;
-              flex-direction: ${compactMode ? `column` : `row`};
-              align-items: center;
-              justify-content: space-around;
-            `}
-          >
-            <div>
-              <h1
-                css={`
-                color: ${theme.surface};
-                ${textStyle('title4')}
-                font-size: ${compactMode ? 6.5 * GU : 7.75 * GU}px;
-                line-height: 1.1;
-                font-weight: 700;
                 margin-bottom: ${2.5 * GU}px;
                 max-width: ${62 * GU}px;
+
+                ${textStyle('title4')}
+                color: ${theme.overlay};
+                font-size: ${wideMode ? '62px' : '52px'};
+                line-height: 1.05;
+                font-weight: 700;
               `}
-              >
-                This is a test organization
-              </h1>
-              <p
+            >
+              This is a test organization
+            </h1>
+            <p
+              css={`
+                ${textStyle('title4')};
+                font-size: ${wideMode ? 2.75 * GU : 2.5 * GU}px;
+                max-width: ${62 * GU}px;
+                color: ${theme.overlay};
+              `}
+            >
+              Aragon Network Cash is a test organization set up as part of a new
+              round of precedence campaign disputes. Learn more on the{' '}
+              <Link
+                href="https://aragon.org/blog/juror-dispute-guide-aragon-network-cash"
                 css={`
-                  ${textStyle('title4')};
-                  font-size: ${compactMode ? 2.5 * GU : 2.75 * GU}px;
-                  max-width: ${62 * GU}px;
-                  color: ${theme.surface};
+                  color: ${theme.overlay};
                 `}
               >
-                Aragon Network Cash is a test organization set up as part of a
-                new round of precedence campaign disputes. Learn more on the{' '}
-                <Link
-                  href="https://aragon.org/blog/juror-dispute-guide-aragon-network-cash"
-                  css={`
-                    color: ${theme.surface};
-                  `}
-                >
-                  Aragon project blog
-                </Link>
-                .
-              </p>
-            </div>
-            {!compactMode && (
-              <img
-                height="401"
-                width="472"
-                src={pcBanner}
-                css={`
-                  max-width: 70%;
-                  margin-top: ${-3 * GU}px;
-                `}
-              />
-            )}
+                Aragon project blog
+              </Link>
+              .
+            </p>
           </div>
-        </LayoutLimiter>
-      </LayoutGutter>
-    </div>
+          {wideMode && (
+            <img
+              height="401"
+              width="472"
+              src={pcSvgBanner}
+              css={`
+                max-width: 70%;
+                margin-top: ${-3 * GU}px;
+              `}
+            />
+          )}
+        </div>
+      </LayoutLimiter>
+    </LayoutGutter>
   )
 }
 
