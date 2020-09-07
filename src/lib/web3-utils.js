2bb9dc88143e6ca0f80b46c70bf0e217796b5191
delfipolito <delfinapolito@gmail.com>
2020-09-07 15:03:04 -0300

    Docs for the shortenAddress function

--- src/lib/web3-utils.js
+++ src/lib/web3-utils.js
@@ -50,6 +50,19 @@ export function addressesEqual(first, second) {
   return first === second
 }
 
+/**
+ * Shorten an Ethereum address. `charsLength` allows to change the number of
+ * characters on both sides of the ellipsis.
+ *
+ * Examples:
+ *   shortenAddress('0x19731977931271')    // 0x1973…1271
+ *   shortenAddress('0x19731977931271', 2) // 0x19…71
+ *   shortenAddress('0x197319')            // 0x197319 (already short enough)
+ *
+ * @param {string} address The address to shorten
+ * @param {number} [charsLength=4] The number of characters to change on both sides of the ellipsis
+ * @returns {string} The shortened address
+ */
 export function shortenAddress(address, charsLength = 4) {
   const prefixLength = 2 // "0x"
   if (!address) {
