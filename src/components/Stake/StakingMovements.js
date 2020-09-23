4be944be05035c55489d985612651271c4a6b7a9
delfipolito <delfinapolito@gmail.com>
2020-09-23 16:20:08 -0300

    Use symbols on statuses

--- src/components/Stake/StakingMovements.js
+++ src/components/Stake/StakingMovements.js
@@ -12,8 +12,82 @@ import {
   useTheme,
   DataView,
 } from '@aragon/ui'
-import { dateFormat, toMs } from '../../utils/date-utils'
 import InternalLink from '../InternalLink'
+import {
+  STAKING_SCHEDULED,
+  STAKING_CHALLENGED,
+  STAKING_COMPLETED,
+  STAKING_CANCELLED,
+  COLLATERAL_LOCKED,
+  COLLATERAL_CHALLENGED,
+  COLLATERAL_AVAILABLE,
+  COLLATERAL_SLASHED,
+  STAKING_STATUSES,
+  COLLATERAL_STATUSES,
+} from './staking-management-statuses'
+import { dateFormat, toMs } from '../../utils/date-utils'
+
+function getActionAttributes(status, theme) {
+  const actionAttributes = {
+    [STAKING_SCHEDULED]: {
+      background: theme.infoSurface,
+      color: theme.tagIndicatorContent,
+      icon: <IconClock size="small" />,
+    },
+    [STAKING_CHALLENGED]: {
+      background: theme.warningSurface,
+      color: theme.warningSurfaceContent,
+      icon: <IconAttention size="small" />,
+    },
+    [STAKING_COMPLETED]: {
+      background: theme.positiveSurface,
+      color: theme.positiveSurfaceContent,
+      icon: <IconCheck size="small" />,
+    },
+    [STAKING_CANCELLED]: {
+      background: theme.surfaceUnder,
+      color: theme.contentSecondary,
+      icon: <IconCross size="small" />,
+    },
+  }
+
+  return actionAttributes[status]
+}
+
+function getCollateralAttributes(status, theme) {
+  const collateralAttributes = {
+    [COLLATERAL_LOCKED]: {
+      color: theme.surfaceOpened,
+      icon: (
+        <IconLock
+          size="small"
+          css={`
+            margin-right: ${1 * GU}px;
+          `}
+        />
+      ),
+    },
+    [COLLATERAL_CHALLENGED]: {
+      color: theme.surfaceOpened,
+      icon: (
+        <IconLock
+          size="small"
+          css={`
+            margin-right: ${1 * GU}px;
+          `}
+        />
+      ),
+    },
+    [COLLATERAL_AVAILABLE]: {
+      color: theme.content,
+    },
+    [COLLATERAL_SLASHED]: {
+      color: theme.negative,
+    },
+  }
+
+  return collateralAttributes[status]
+}
 
 function StakingMovements({ stakingMovements, token }) {
   const theme = useTheme()
@@ -36,58 +110,15 @@ function StakingMovements({ stakingMovements, token }) {
         tokenDecimals,
         disputableActionId,
       }) => {
-        const actionAttributes = {
-          Scheduled: {
-            background: theme.infoSurface,
-            color: theme.tagIndicatorContent,
-            icon: <IconClock size="small" />,
-          },
-          Challenged: {
-            background: theme.warningSurface,
-            color: theme.warningSurfaceContent,
-            icon: <IconAttention size="small" />,
-          },
-          Completed: {
-            background: theme.positiveSurface,
-            color: theme.positiveSurfaceContent,
-            icon: <IconCheck size="small" />,
-          },
-          Cancelled: {
-            background: theme.surfaceUnder,
-            color: theme.contentSecondary,
-            icon: <IconCross size="small" />,
-          },
-        }
-        const amountAttributes = {
-          Locked: {
-            color: theme.surfaceOpened,
-            icon: (
-              <IconLock
-                size="small"
-                css={`
-                  margin-right: ${1 * GU}px;
-                `}
-              />
-            ),
-          },
-          Challenged: {
-            color: theme.surfaceOpened,
-            icon: (
-              <IconLock
-                size="small"
-                css={`
-                  margin-right: ${1 * GU}px;
-                `}
-              />
-            ),
-          },
-          Available: {
-            color: theme.content,
-          },
-          Slashed: {
-            color: theme.negative,
-          },
-        }
+        const stakingStatus = STAKING_STATUSES.get(actionState)
+        const actionAttributes = getActionAttributes(stakingStatus, theme)
+
+        const collateralStatus = COLLATERAL_STATUSES.get(collateralState)
+        const amountAttributes = getCollateralAttributes(
+          collateralStatus,
+          theme
+        )
+
         return [
           <time
             dateTime={dateFormat(toMs(createdAt), 'standard')}
@@ -98,14 +129,10 @@ function StakingMovements({ stakingMovements, token }) {
           <div>
             <Tag
               background={
-                actionAttributes[actionState].background &&
-                `${actionAttributes[actionState].background}`
+                actionAttributes.background && `${actionAttributes.background}`
               }
-              color={
-                actionAttributes[actionState].color &&
-                `${actionAttributes[actionState].color}`
-              }
-              icon={actionAttributes[actionState].icon}
+              color={actionAttributes.color && `${actionAttributes.color}`}
+              icon={actionAttributes.icon}
               mode="indicator"
               label={actionState}
             />
@@ -119,12 +146,12 @@ function StakingMovements({ stakingMovements, token }) {
           <span
             css={`
               font-weight: 600;
-              color: ${amountAttributes[collateralState].color};
+              color: ${amountAttributes.color};
               display: flex;
               align-items: center;
             `}
           >
-            {amountAttributes[collateralState].icon}
+            {amountAttributes.icon}
             {formatTokenAmount(amount, tokenDecimals, { symbol: token.symbol })}
           </span>,
         ]
